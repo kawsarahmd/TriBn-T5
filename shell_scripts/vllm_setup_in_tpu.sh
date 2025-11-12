d0381abed9977b6c1c5b513cd7b8bf617f4f29d0
Kawsar Ahmed <kawsar.ahmd@gmail.com>
2025-11-13 00:46:18 +0600

    Create vllm_setup_in_tpu.sh

---
+++ shell_scripts/vllm_setup_in_tpu.sh
@@ -0,0 +1,26 @@
+git clone https://github.com/vllm-project/vllm.git && cd vllm
+pip uninstall torch torch-xla -y
+pip install -r requirements/tpu.txt
+
+sudo apt-get install --no-install-recommends --yes libopenblas-base libopenmpi-dev libomp-dev
+VLLM_TARGET_DEVICE="tpu" python -m pip install -e .
+
+pip install "transformers[sentencepiece]"
+
+#since breaking change in version 4. scripts are no longer supported
+pip install datasets==3.6.0
+
+pip install accelerate
+
+pip install evaluate
+
+pip install nltk
+
+sudo apt-get install --no-install-recommends --yes libopenblas-base libopenmpi-dev libomp-dev
+
+
+pip install optax
+
+pip install flax
+
+pip install rouge_score
